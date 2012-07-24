70f462458e3deadd51ae30d2d6e0d96f2c79ef28
Mickael Kerbrat <stolati@gmail.com>
2012-07-24 22:43:48 +0200

    trying to set MongoDB

---
+++ app/models/MapDB.scala
@@ -0,0 +1,13 @@
+package models
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: me
+ * Date: 7/16/12
+ * Time: 8:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class MapDB {
+
+}
--- project/Build.scala
+++ project/Build.scala
@@ -8,20 +8,30 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     //I don't know in which one I have to put it
+    val casbah = "com.mongodb.casbah" %% "casbah" % "2.1.5.0"
 
     val appDependencies = Seq(
       "org.jfarcand" % "wcs" % "1.2",
-      "net.vz.mongodb.jackson" %% "play-mongo-jackson-mapper" % "1.0.0"
+      //"net.vz.mongodb.jackson" %% "play-mongo-jackson-mapper" % "1.0.0",
+      "se.radley" %% "play-plugins-salat" % "1.0.7"
     )
 
+  /*
     val libraryDependencies = Seq(
       "org.jfarcand" % "wcs" % "1.2",
-      "net.vz.mongodb.jackson" %% "play-mongo-jackson-mapper" % "1.0.0"
+      //"net.vz.mongodb.jackson" %% "play-mongo-jackson-mapper" % "1.0.0",
+      "com.mongodb.casbah" %% "casbah" % "2.1.5.0",
+      "se.radley" %% "play-plugins-salat" % "1.0.7"
     )
+    */
 
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+      routesImport += "se.radley.plugin.salat.Binders._",
+      templatesImport += "org.bson.types.ObjectId"
     )
+
+
+
 
 }
