f471e090fb3a83cc660c0854b9592d9c1b630d81
Mickael Kerbrat <stolati@gmail.com>
2012-08-06 20:32:22 +0200

    added state to the PlayerLink element

--- app/models/MapRoom.scala
+++ app/models/MapRoom.scala
@@ -17,12 +17,12 @@ case class HasIdJump(id : Id)
 
 class PlayerLink() extends Actor {
 
-  //object PlayerLinkState extends Enumeration {
-  //  type PlayerLinkState = Value
-  //  val notLinkedYet, linkedToMap, changingServer = Value
-  //}
+  object PlayerLinkState extends Enumeration {
+    type PlayerLinkState = Value
+    val notLinkedYet, linkedToMap, changingServer = Value
+  }
 
-  //var state = PlayerLinkState.notLinkedYet
+  var state = PlayerLinkState.notLinkedYet
 
 
   var body : Option[Body] = None
@@ -34,114 +34,191 @@ class PlayerLink() extends Actor {
   var movingServName : Option[String]  = None
   var movingFuturePlayer : Option[PlayerJumpingInit] = None
 
-
-  def setWs(actor : Actor) { this.wsLink = Some(actor) }
-
-  //def act_state_NotLinkedYet(a : Any) = a match {
-  //  case _ => "TODO"
-
-
-  //}
-
-
-  //def act_state_linkedToMap(a : Any) = a match {
-  //  case _ => "TODO"
-
-
-  //}
-
-
-  //def act_state_changingServer(a : Any) = a match {
-
-  //  case HasIdJump(id)  =>
-  //    val fp = Servers.futurePlayers(id)
-  //    Servers.futurePlayers -= id
-  //    movingFuturePlayer = Some(fp)
-
-
-
-
-  //}
-
-
-
   def act = {
 
     loop {
-      receive {
-
-        case HasIdJump(id)  =>
-          val fp = Servers.futurePlayers(id)
-          Servers.futurePlayers -= id
-          movingFuturePlayer = Some(fp)
-
-        case FROM_LINK(Ask_Map()) =>
-          if(movingFuturePlayer != None){
-            val mfp = movingFuturePlayer.get
-
-            mapLink = Some(MapRoom.getMap(mfp.mapName))
-            mapLink.get ! PlayerJoin(mfp.pos)
-
-          } else {
-            assert(mapLink == None)
-
-
-            val (defMapName, defPos) = MapRoom.defaultMapInfo
-            mapLink = Some(MapRoom.getMap(defMapName))
-
-            mapLink.get ! PlayerJoin(defPos)
-          }
-
-        case ChangeMap(mapRoom, pos) =>
-          if(mapLink != None){
-            mapLink.get ! Quit(this.body.get.id)
-            wsLink.get ! YouQuit()
-          }
-
-          mapLink = Some(mapRoom)
-          mapLink.get ! PlayerJoin(pos)
-
-        case cm : CurrentMap =>
-          body = Some(cm.your_body)
-          wsLink.get ! cm
-
-        case FROM_LINK(mm : Me_Move) => mapLink.get ! (body.get.id, mm)
-
-        case e : CONNECTION_END =>
-          //wsLink = None
-          this.mapLink.get ! Quit(this.body.get.id)
-          exit
-
-        //se we wait for the map to know we are no more
-        //and other element can make us quitting
-        case Player_Quit(id) =>
-          if(id != body.get.id) wsLink.get ! Player_Quit(id)
-
-        case pm : Player_Move => wsLink.get ! pm
-        case pj : Player_Join => wsLink.get ! pj
-        //case q : Quit => wsLink.get ! YouQuit()
-
-        case FloorServerJump(servName, mapName, pos) =>
-          mapLink.get ! Quit(body.get.id)
-          wsLink.get ! YouQuit()
-
-          servLink = Some(models.Servers.getServerLink(servName, this))
-
-          servLink.get ! PlayerJumpingInit(mapName, pos)
-
-          movingServName = Some(servName)
-
-        case FROM_LINK(PlayerJumpingId(id)) => id
-          val url = Servers.getMoveUrl(movingServName.get, id)
-          wsLink.get ! YouJump(url)
-
-          wsLink.get ! 'quit
-          exit
-
-        case x : Any => println("ClientActor receive : ", x)
-
+      receive { case a : Any =>
+        state match {
+          case PlayerLinkState.notLinkedYet => act_state_NotLinkedYet(a)
+          case PlayerLinkState.linkedToMap => act_state_linkedToMap(a)
+          case PlayerLinkState.changingServer => act_state_changingServer(a)
+        }
       }
     }
+  }
+
+  def setWs(actor : Actor) { this.wsLink = Some(actor) }
+
+  def act_state_NotLinkedYet(a : Any) = a match {
+
+    case HasIdJump(id)  =>
+      val fp = Servers.futurePlayers(id)
+      Servers.futurePlayers -= id
+      movingFuturePlayer = Some(fp)
+
+    case FROM_LINK(Ask_Map()) =>
+      if(movingFuturePlayer != None){
+        val mfp = movingFuturePlayer.get
+
+        mapLink = Some(MapRoom.getMap(mfp.mapName))
+        mapLink.get ! PlayerJoin(mfp.pos)
+
+      } else {
+        assert(mapLink == None)
+
+
+        val (defMapName, defPos) = MapRoom.defaultMapInfo
+        mapLink = Some(MapRoom.getMap(defMapName))
+
+        mapLink.get ! PlayerJoin(defPos)
+      }
+      state = PlayerLinkState.linkedToMap
+
+
+    case ChangeMap(mapRoom, pos) =>
+      if(mapLink != None){
+        mapLink.get ! Quit(this.body.get.id)
+        wsLink.get ! YouQuit()
+      }
+
+      mapLink = Some(mapRoom)
+      mapLink.get ! PlayerJoin(pos)
+
+    case cm : CurrentMap =>
+      body = Some(cm.your_body)
+      wsLink.get ! cm
+
+    case FROM_LINK(mm : Me_Move) => mapLink.get ! (body.get.id, mm)
+
+    case e : CONNECTION_END =>
+      //wsLink = None
+      this.mapLink.get ! Quit(this.body.get.id)
+      exit
+
+    //se we wait for the map to know we are no more
+    //and other element can make us quitting
+    case Player_Quit(id) =>
+      if(id != body.get.id) wsLink.get ! Player_Quit(id)
+
+    case pm : Player_Move => wsLink.get ! pm
+    case pj : Player_Join => wsLink.get ! pj
+    //case q : Quit => wsLink.get ! YouQuit()
+
+    case FloorServerJump(servName, mapName, pos) =>
+      mapLink.get ! Quit(body.get.id)
+      wsLink.get ! YouQuit()
+
+      servLink = Some(models.Servers.getServerLink(servName, this))
+
+      servLink.get ! PlayerJumpingInit(mapName, pos)
+
+      movingServName = Some(servName)
+
+    case FROM_LINK(PlayerJumpingId(id)) => id
+    val url = Servers.getMoveUrl(movingServName.get, id)
+    wsLink.get ! YouJump(url)
+
+    wsLink.get ! 'quit
+    exit
+
+    case x : Any => println("ClientActor receive : ", x)
+
+
+
+  }
+
+  def act_state_linkedToMap(a : Any) = a match {
+
+    case FROM_LINK(Ask_Map()) =>
+      if(movingFuturePlayer != None){
+        val mfp = movingFuturePlayer.get
+
+        mapLink = Some(MapRoom.getMap(mfp.mapName))
+        mapLink.get ! PlayerJoin(mfp.pos)
+
+      } else {
+        assert(mapLink == None)
+
+
+        val (defMapName, defPos) = MapRoom.defaultMapInfo
+        mapLink = Some(MapRoom.getMap(defMapName))
+
+        mapLink.get ! PlayerJoin(defPos)
+      }
+
+
+    case ChangeMap(mapRoom, pos) =>
+      if(mapLink != None){
+        mapLink.get ! Quit(this.body.get.id)
+        wsLink.get ! YouQuit()
+      }
+
+      mapLink = Some(mapRoom)
+      mapLink.get ! PlayerJoin(pos)
+
+    case cm : CurrentMap =>
+      body = Some(cm.your_body)
+      wsLink.get ! cm
+
+    case FROM_LINK(mm : Me_Move) => mapLink.get ! (body.get.id, mm)
+
+    case e : CONNECTION_END =>
+      //wsLink = None
+      this.mapLink.get ! Quit(this.body.get.id)
+      exit
+
+    //se we wait for the map to know we are no more
+    //and other element can make us quitting
+    case Player_Quit(id) =>
+      if(id != body.get.id) wsLink.get ! Player_Quit(id)
+
+    case pm : Player_Move => wsLink.get ! pm
+    case pj : Player_Join => wsLink.get ! pj
+    //case q : Quit => wsLink.get ! YouQuit()
+
+    case FloorServerJump(servName, mapName, pos) =>
+      mapLink.get ! Quit(body.get.id)
+      wsLink.get ! YouQuit()
+
+      servLink = Some(models.Servers.getServerLink(servName, this))
+
+      servLink.get ! PlayerJumpingInit(mapName, pos)
+
+      movingServName = Some(servName)
+      state = PlayerLinkState.changingServer
+
+    case FROM_LINK(PlayerJumpingId(id)) => id
+    val url = Servers.getMoveUrl(movingServName.get, id)
+    wsLink.get ! YouJump(url)
+
+    wsLink.get ! 'quit
+    exit
+
+    case x : Any => println("ClientActor receive : ", x)
+
+
+  }
+
+  def act_state_changingServer(a : Any) = a match {
+
+    case e : CONNECTION_END =>
+      //wsLink = None
+      this.mapLink.get ! Quit(this.body.get.id)
+      exit
+
+    //se we wait for the map to know we are no more
+    //and other element can make us quitting
+    case Player_Quit(id) =>
+      if(id != body.get.id) wsLink.get ! Player_Quit(id)
+
+    case FROM_LINK(PlayerJumpingId(id)) => id
+      val url = Servers.getMoveUrl(movingServName.get, id)
+      wsLink.get ! YouJump(url)
+
+      wsLink.get ! 'quit
+      exit
+
+    case x : Any => println("ERROR : ClientActor receive : ", x)
   }
 
 }
