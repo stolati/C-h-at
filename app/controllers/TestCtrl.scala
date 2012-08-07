8c2ab838ffafeeb3bbd20b68f1cee08e3c48c393
Mickael Kerbrat <stolati@gmail.com>
2012-08-07 23:20:49 +0200

    preparing for user

---
+++ app/controllers/TestCtrl.scala
@@ -0,0 +1,13 @@
+package controllers
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: me
+ * Date: 8/6/12
+ * Time: 9:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class TestCtrl {
+
+}
---
+++ app/models/PlayerLink.scala
@@ -0,0 +1,13 @@
+package models
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: me
+ * Date: 8/7/12
+ * Time: 10:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class PlayerLink {
+
+}
